d7f47e49e455aa9cf680d32c5f0d3f60e4e1e74e
Rohan Somadder <rohansomadder10@gmail.com>
2021-11-22 22:24:24 +0530

    main updated

--- Main.py
+++ Main.py
@@ -2,11 +2,8 @@ import random
 
 
 def Random_num():
+    # Generates random number between 1 and 10
     return random.randint(1, 10)
-
-
-def evaluate():
-    pass
 
 
 def main():
@@ -16,8 +13,19 @@ def main():
     while cont:
         num = Random_num()
         n = int(input("Enter a number : "))
-        if evaluate(n, num):
-            pass
+
+        if abs(n - num) <= 1:
+            print("You were very close try again!!!")
+
+        elif n == num:
+            print("Correct!!! Wery well played.....")
+
+        else:
+            print("Wrong guess , better luck next time!!!")
+
+        i = input("Do you want to play again ? (Y/N)")
+        if i.lower() == 'y':
+            cont = True
 
 
 if __name__ == '__main__':
