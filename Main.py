05166dfd359130e2f34e546cdc6870b29b5698f9
Rohan Somadder <rohansomadder10@gmail.com>
2021-11-22 22:07:00 +0530

    main updated

--- Main.py
+++ Main.py
@@ -5,12 +5,19 @@ def Random_num():
     return random.randint(1, 10)
 
 
+def evaluate():
+    pass
+
+
 def main():
     # Todo: to print some welcome message
     # print("Welcome")
     cont = True
     while cont:
         num = Random_num()
+        n = int(input("Enter a number : "))
+        if evaluate(n, num):
+            pass
 
 
 if __name__ == '__main__':
