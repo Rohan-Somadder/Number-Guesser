5a4ee0562ace0d20bf1f1c66e9ccac1686cf5c0e
Rohan Somadder <rohansomadder10@gmail.com>
2021-11-23 21:51:36 +0530

    Main.py updated

--- Main.py
+++ Main.py
@@ -6,29 +6,37 @@ def Random_num():
     return random.randint(1, 9)
 
 
+def calc(guess, no, s=0):
+    if abs(guess - no) <= 1:
+        print("\nYou were very close try again!!!")
+
+    elif guess == no:
+        print("\nCorrect!!! Wery well played.....")
+        if s != 1:
+            try:
+                no = int(input("\nEnter a number again : "))
+            except ValueError:
+                print("Your input is not a number, try with a number only!!!")
+            calc(guess, no, 1)
+
+    else:
+        print(f"\nWrong guess , the answer was {no} better luck next time!!!")
+
+
 def main():
     # Todo: to print some welcome message
     # print("Welcome")
     cont = True
     while cont:
         num = Random_num()
-        n = int(input("\nEnter a number : "))
+        try:
+            n = int(input("\nEnter a number : "))
+        except ValueError:
+            print("Your input is not a number, try with a number only!!!")
 
-        if abs(n - num) <= 1:
-            print("\nYou were very close try again!!!")
-
-        elif n == num:
-            print("\nCorrect!!! Wery well played.....")
-
-        else:
-            print(
-                f"\nWrong guess , the answer was {num} better luck next time!!!")
-
+        calc(n, num)
         i = input("\nDo you want to play again ? (Y/N)")
-        if i.lower() == 'y':
-            cont = True
-        else:
-            cont = False
+        cont = bool(i.lower() == 'y')
 
 
 if __name__ == '__main__':
